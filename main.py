eb6318629304090029fb4325724a715c74764d48
AlexKlos <klos@list.ru>
2024-11-26 22:32:49 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,82 @@
+import json
+import os
+from pprint import pprint
+import requests
+
+import folium
+from dotenv import load_dotenv
+from geopy import distance
+
+
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lon, lat
+
+
+def get_distance(coffee):
+    return coffee['distance']
+
+
+def main():
+    load_dotenv()
+    yandex_api_key = os.getenv('YANDEX_API_KEY')
+
+    place = input('Где вы находитесь? ')
+    user_coordinates = fetch_coordinates(yandex_api_key, place)
+    reversed_user_coordinates = (
+        float(user_coordinates[1]), 
+        float(user_coordinates[0])
+        )
+    print('Ваши координаты:', user_coordinates)
+
+    with open('coffee.json', 'r') as coffee_file:
+        coffee_file_contents = json.loads(coffee_file.read())
+
+    coffees = []
+
+    for coffee in coffee_file_contents:
+        coffee_coordinates = (
+            float(coffee['geoData']['coordinates'][1]), 
+            float(coffee['geoData']['coordinates'][0])
+            )
+        distance_to_coffee = distance.distance(reversed_user_coordinates, 
+                                               coffee_coordinates).km
+        normalized_coffee = {
+            'title': coffee['Name'], 
+            'distance': distance_to_coffee, 
+            'latitude': coffee['geoData']['coordinates'][1], 
+            'longitude': coffee['geoData']['coordinates'][0]
+            }
+        coffees.append(normalized_coffee)
+
+    sorted_coffees = sorted(coffees, key=get_distance)
+    pprint(sorted_coffees[:5])
+
+    user_map = folium.Map(reversed_user_coordinates)
+
+    for coffee in sorted_coffees[:5]:
+        folium.Marker(
+            location=[coffee['latitude'], coffee['longitude']],
+            tooltip='Click me!',
+            popup=coffee['title'],
+            icon=folium.Icon(color='green')
+        ).add_to(user_map)
+
+    user_map.save('index.html')
+
+
+if __name__ == '__main__':
+    main()
